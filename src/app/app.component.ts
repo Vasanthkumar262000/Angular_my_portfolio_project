1ceca66ccdd7d0c1b2b77579d5633a62e5834a47
Vasanthkumar R <vasanthkumarr@Vasanthkumars-MacBook-Pro.local>
2024-12-16 23:34:56 -0600

    Added Skill section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import {faHome} from '@fortawesome/free-solid-svg-icons';
-import {faFilePdf} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faJava, faLinkedin, faPython, faGithub, faJs, faReact, faSquareJs, faBootstrap} from '@fortawesome/free-brands-svg-icons';
+import { faGlobe,faFilePdf,faHome, faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -19,10 +15,17 @@ export class AppComponent {
   title = 'my_portfolio_project';
 
   constructor(library: FaIconLibrary) {
-    library.addIcons(faLinkedin); // Add all solid icons to the library
+    library.addIcons(faLinkedin);
     library.addIcons(faGithub);
     library.addIcons(faGlobe);
     library.addIcons(faHome);
     library.addIcons(faFilePdf);
+    library.addIcons(faPython);
+    library.addIcons(faJava);
+    library.addIcons(faJs);
+    library.addIcons(faReact);
+    library.addIcons(faSquareJs);
+    library.addIcons(faBootstrap);
+    library.addIcons(faDatabase);
   }
 }
