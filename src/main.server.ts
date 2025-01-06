ef9b55d1a6968c25330efba8acf6fea7df90623c
Vasanthkumar R <vasanthkumarr@Vasanthkumars-MacBook-Pro.local>
2025-01-05 22:52:38 -0600

    new update

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { environment } from './environments/environment';
+
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
